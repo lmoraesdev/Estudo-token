ebbe2f5d3af8790a7f42758b3306c52d245259bb
lmoraesdev <lbc_moraes@outlook.com>
2023-01-23 16:05:56 -0300

    feat: criaçao de assinatura

---
+++ assinatura/assinatura.js
@@ -0,0 +1,30 @@
+import { generateKeyPairSync, createSign, createVerify } from "crypto";
+
+const { privateKey, publicKey } = generateKeyPairSync("rsa", {
+	modulusLength: 2048,
+
+	publicKeyEncoding: {
+		type: "spki",
+		format: "pem",
+	},
+	privateKeyEncoding: {
+		type: "pkcs8",
+		format: "pem",
+	},
+});
+
+const dados = "Essa string vai ser assinada";
+
+const assinador = createSign("rsa-sha256");
+assinador.update(dados);
+
+const assinatura = assinador.sign(privateKey, "hex");
+
+console.log(`Assinatura:${assinatura}`);
+
+const verificador = createVerify("rsa-sha256");
+verificador.update(dados);
+
+const ehVerificado = verificador.verify(publicKey, assinatura, "hex");
+
+console.log(ehVerificado);
